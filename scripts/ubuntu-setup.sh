c64f8d85d45bd3a198ded4a4793a134ae07b6ccb
Igor Prieto <ipechepare@gmail.com>
2023-02-26 11:21:09 +0100

    Customization of the original dotfiles

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+wget -qO - https://regolith-desktop.org/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
 
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-ubuntu-kinetic-amd64 kinetic main" | \
+sudo tee /etc/apt/sources.list.d/regolith.list
 
-sudo dnf install -y \
+sudo apt update
+sudo apt install regolith-desktop regolith-compositor-picom-glx
+sudo apt upgrade
+
+sudo apt install -y \
     anacron \
     cargo \
     gnome-tweaks \
@@ -15,19 +22,19 @@ sudo dnf install -y \
     kitty \
     zsh \
     g++ \
-    lua-devel \
+#    lua-devel \
     luarocks \
     php \
     php-bcmath \
     php-mysqlnd \
-    php-pecl-zip \
+#    php-pecl-zip \
     php-pgsql \
     php-posix \
     php-soap \
-    docker-ce \
+#    docker-ce \
     docker-ce-cli \
-    containerd.io \
-    docker-compose-plugin \
+#    containerd.io \
+#    docker-compose-plugin \
     bat \
     fzf \
     httpie \
@@ -36,17 +43,16 @@ sudo dnf install -y \
     ruby \
     python3 \
     htop \
-    proselint \
-    lm_sensors \
-    discord \
-    slack
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.telegram.desktop
+#    proselint \
+#    lm_sensors \
+#    discord \
+    slack \
+    ruby \
+    luarocks
 
 sudo gem install lolcat
 
-pip install gitlint
+sudo pip install gitlint
 
 sudo luarocks install luacheck
 
@@ -68,3 +74,5 @@ sudo systemctl enable containerd.service
 sudo systemctl start docker
 
 echo "Don't forget to copy over your .ssh and .gnupg directories!"
+
+sudo reboot
