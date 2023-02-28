58350f2c1b9e3d69e42f3953646c11b7cc00a556
Igor Prieto <ipechepare@gmail.com>
2023-03-01 00:08:35 +0100

    lasts settings up

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+apt update
+apt install sudo
+adduser igorpri3to
+usermod -aG sudo igorpri3to
+su igorpri3to
+
 sudo apt install -y \
     wget \
     gpg
@@ -26,20 +32,15 @@ sudo apt install -y \
     kitty \
     zsh \
     g++ \
-#    lua-devel \
+    lua5.4 \
     luarocks \
     php \
     php-bcmath \
     php-mysqlnd \
-#    php-pecl-zip \
+    php8.1-zip \
     php-pgsql \
-    php-posix \
     php-soap \
     php-xml \
-#    docker-ce \
-    docker-ce-cli \
-#    containerd.io \
-#    docker-compose-plugin \
     bat \
     fzf \
     httpie \
@@ -47,22 +48,34 @@ sudo apt install -y \
     tmux \
     ruby \
     python3 \
+    pip \
     htop \
-#    proselint \
-#    lm_sensors \
-#    discord \
-    slack \
-    ruby \
-    luarocks \
+    lm-sensors \
     curl \
-    php-curl
+    php-curl \
+    ca-certificates \
+    lsb-release
+
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
+
+RIPGREP_VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[0-9.]+')
+curl -Lo ripgrep.deb "https://github.com/BurntSushi/ripgrep/releases/latest/download/ripgrep_${RIPGREP_VERSION}_amd64.deb"
+sudo apt install -y ./ripgrep.deb
+rg --version
+rm -rf ripgrep.deb
 
 sudo gem install lolcat
-
-sudo pip install gitlint
-
+pip install gitlint
+pip install proselint
 sudo luarocks install luacheck
-
 cargo install stylua
 
 # Composer
